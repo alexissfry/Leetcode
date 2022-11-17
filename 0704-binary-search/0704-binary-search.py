a0983e7a94c819557f7ee402211d58b4e1d33c03
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2022-11-17 01:25:27 -0500

    Time: 706 ms (7.46%), Space: 15.5 MB (74.17%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -5,14 +5,14 @@ class Solution:
         right = len(nums)-1
         
         while left <= right:
-            pivot = (left+right)//2
-            
-            if nums[pivot] == target:
+            pivot = (right + left)//2 
+            if target == nums[pivot]:
                 return pivot
             elif target < nums[pivot]:
-                right = pivot - 1
-            
-            else: 
-                left = pivot + 1
-            
+                right = pivot-1
+            else:
+                left = pivot+1
         return -1
+                
+                
+            
