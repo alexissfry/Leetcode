8e4ccb73e6c03190f2d1a82b87861166fb3f371a
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2022-11-21 14:33:44 -0500

    Time: 137 ms (52.32%), Space: 15.2 MB (26.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,14 +2,13 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         history = {}
         
-        for i, num in enumerate(nums):
-            complement = target - num
-            
-            if complement in history:
-                return [i, history[complement]]
+        for i, e in enumerate(nums):
+            comp = target - e
+            if comp in history:
+                return [i,history[comp]]
             else:
-                history[num] = i
-                
+                history[e] = i 
+            
         return []
     
     
