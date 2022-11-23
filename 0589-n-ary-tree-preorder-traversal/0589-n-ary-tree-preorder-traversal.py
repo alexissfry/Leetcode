227c2265a5f5f48e51e90b2cca08a4c3a8e1c2c1
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2022-11-23 03:59:30 -0500

    Time: 72 ms (62.69%), Space: 16.2 MB (98.63%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        
+        if root:
+            pre = [root.val]
+            for node in root.children:
+                pre += self.preorder(node)
+            
+            return pre
+            
