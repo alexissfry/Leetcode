a7510b3302112f281c217f955b8486650db4b364
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-01-07 19:20:57 -0500

    Time: 37 ms (70.84%), Space: 13.9 MB (15.13%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -4,7 +4,7 @@ class Solution:
         pascale.append([1])
         rows = 1
 
-        while rows < rowIndex+1:
+        while rows <= rowIndex:
             pascale.append([1])
             for i in range(len(pascale[rows-1])-1):
                 pascale[rows].append(pascale[rows-1][i] + pascale[rows-1][i+1])
