590c8d34324df873149c3e2dc30a57c4337a1b63
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-02-06 15:30:38 -0500

    Time: 253 ms (50.26%), Space: 21.2 MB (69.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -14,6 +14,33 @@ class Solution:
         
         return result 
         
+'''
+[1,2,3,4]
+
+i = 0
+result=[1,1,1,1]
+pre = 1
+result=[1,1,1,1]
+post = 4
+
+i = 1 
+result=[1,1,1,1]
+pre = 2
+result=[1,1,4,1]
+post = 12
+
+i = 2
+result=[1,1,8,1]
+pre = 6
+result=[1,12,8,1]
+post = 24
+
+i = 3
+result=[1,12,8,6]
+pre = 6
+result=[24,12,4,1]
+post = 24
+'''
         
         
             
