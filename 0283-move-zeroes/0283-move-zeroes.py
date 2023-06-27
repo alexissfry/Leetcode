f6b32491d651cbf1735d24f079867fe2f62c3efd
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-06-27 01:15:03 -0400

    Time: 161 ms (98.06%), Space: 18 MB (12.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,12 +4,12 @@ class Solution:
         
         for i in range(len(nums)):
             if nums[i] != 0:
-                j=i
-                while j > nonzero:
-                    temp = nums[j]
-                    nums[j] = nums[j-1]
-                    nums[j-1] = temp
-                    j -= 1
+                #j=i
+                #while j > nonzero:
+                temp = nums[nonzero]
+                nums[nonzero] = nums[i]
+                nums[i] = temp
+                    #j -= 1
                 nonzero += 1
             
         
