9d11b88e2fda23be72eb3904bba82e31b270671d
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-06-30 00:15:23 -0400

    Time: 570 ms (71.39%), Space: 17.1 MB (42.92%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -3,22 +3,20 @@ class Solution:
         
         maxOnes = 0
         currOnes = 0
-        avail = k
-        i = 0
         
-        while i < len(nums):
+        for i in range(len(nums)):
             
             if nums[i] == 1:
                 currOnes += nums[i]
-            elif avail == 0:
+                maxOnes = max(maxOnes, currOnes)
+            elif k == 0:
                 while nums[i-currOnes] == 1:
                     currOnes -= 1
             else:
                 currOnes += 1
-                avail -= 1
-                
-            maxOnes = max(maxOnes, currOnes)
-            i += 1
+                k -= 1
+                maxOnes = max(maxOnes, currOnes)
+            
         
         return maxOnes
                 
