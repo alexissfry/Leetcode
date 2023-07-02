5ca5f4b3c853ab4f28f94bf97360010367dd9f7f
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-07-02 14:34:56 -0400

    Time: 42 ms (96.79%), Space: 16.3 MB (89.29%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,7 +1,13 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        dictNums = Counter(arr)
+        dictNums = {}
         occur = set()
+        
+        for i in arr:
+            if i in dictNums:
+                dictNums[i] += 1
+            else:
+                dictNums[i] = 1
         
         for i in dictNums.values():
             if i in occur:
