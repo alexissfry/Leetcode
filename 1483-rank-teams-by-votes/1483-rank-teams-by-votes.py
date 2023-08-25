fc1e3cb872aa657a5af1e0e61228570e5008a740
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-08-25 17:49:07 -0400

    Time: 83 ms (70.03%), Space: 16.5 MB (99.41%) - LeetHub

--- 1483-rank-teams-by-votes/1483-rank-teams-by-votes.py
+++ 1483-rank-teams-by-votes/1483-rank-teams-by-votes.py
@@ -2,26 +2,23 @@ class Solution:
     def rankTeams(self, votes: List[str]) -> str:
         voteCount = {}
 
-        for v in votes:
+        for v in votes: 
             for i in range(len(v)):
                 if v[i] in voteCount:
-                    voteCount[v[i]][i] -= 1
+                    voteCount[v[i]][i] += -1 
                 else:
                     voteCount[v[i]] = [0] * len(v)
-                    voteCount[v[i]][i] = -1
-
-        print(voteCount)
+                    voteCount[v[i]][i] = -1 
         
-        allTallies = []
-        for a,b in voteCount.items():
-            allTallies.append((b,a))
+        candidates = []
 
-        allTallies.sort()
-        result = "" 
-        print(allTallies)
-        for n in range(0,len(allTallies)):
-            result += allTallies[n][1]
+        for a,b in voteCount.items():
+            candidates.append((b,a))
+
+        candidates.sort()
+
+        result = ""
+        for c in candidates:
+            result += c[1]
         
         return result
-
-                
