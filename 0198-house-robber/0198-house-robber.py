c9ac326b01f4e9d8d288f39fad9f9e778c3fd214
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-09-14 02:28:09 -0400

    Time: 41 ms (53.81%), Space: 16.1 MB (92.91%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,14 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        if len(nums) == 1:
+        path = [0] * len(nums)
+
+        if len(path) == 1:
             return nums[0]
+
+        path[0] = nums[0]
+        path[1] = max(nums[0],nums[1])
+        print(path)
+
+        for i in range(2,len(nums)):
+            path[i] = max(path[i-1], nums[i]+path[i-2])
         
-        maxRob = [nums[0],max(nums[0],nums[1])]
-        
-        for i in range(2, len(nums)):
-            rob = max(maxRob[i-1],maxRob[i-2]+nums[i])
-            
-            maxRob.append(rob)
-        
-        return maxRob[-1]
+        return path[-1]
