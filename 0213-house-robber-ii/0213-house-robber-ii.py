ec98afbc33804ab03e5ad6b8e728b80ddde53897
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-09-20 01:08:47 -0400

    Time: 25 ms (99.73%), Space: 16.2 MB (92.78%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,19 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
 
-        def dp(nums):
-        
-            maxStart = max(nums[0],nums[1])
-            money = [nums[0], maxStart]
+        def getRobbed(nums):
+            robbed = [0] * len(nums)
+            robbed[0] = nums[0]
+            robbed[1] = max(nums[0],nums[1])
 
             for i in range(2,len(nums)):
-                money.append(max(money[i-2]+nums[i],money[i-1]))
+                robbed[i] = max(robbed[i-1], robbed[i-2]+nums[i])
 
-            return money[-1]
+            return robbed
 
         if len(nums) == 1:
             return nums[0]
         if len(nums) == 2:
             return max(nums[0],nums[1])
 
-        return max(dp(nums[1:]), dp(nums[:-1]))
+        return max(getRobbed(nums[1:])[-1], getRobbed(nums[:len(nums)-1])[-1])
+
