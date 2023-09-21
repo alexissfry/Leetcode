7aac20ac366ca143a95136eee82b4ce9bf7c79f0
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-09-21 14:11:01 -0400

    Time: 69 ms (73%), Space: 17.3 MB (32.58%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.py
+++ 0871-keys-and-rooms/0871-keys-and-rooms.py
@@ -1,11 +1,8 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-
         visited = [False] * len(rooms)
-        
+
         def checkRooms(index):
-            #print(index, visited)
-            nonlocal rooms
 
             if visited[index] == True:
                 return 
@@ -13,12 +10,10 @@ class Solution:
             visited[index] = True 
             for key in rooms[index]:
                 checkRooms(key)
-
+            
         checkRooms(0)
 
         for v in visited:
             if v == False:
                 return False
-        return True
-            
-
+        return True 
