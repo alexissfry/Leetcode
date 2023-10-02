4ab4e6b62229a48d2485fda13ac4fa3eaa5bab49
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-10-02 13:29:54 -0400

    Time: 233 ms (37.86%), Space: 17.7 MB (96.24%) - LeetHub

---
+++ 1905-design-authentication-manager/1905-design-authentication-manager.py
@@ -0,0 +1,27 @@
+class AuthenticationManager:
+
+    def __init__(self, timeToLive: int):
+        self.time = timeToLive
+        self.tokens = dict()
+        
+
+    def generate(self, tokenId: str, currentTime: int) -> None:
+        self.tokens[tokenId] = currentTime 
+
+    def renew(self, tokenId: str, currentTime: int) -> None:
+        if tokenId in self.tokens and self.tokens[tokenId] + self.time > currentTime:
+            self.tokens[tokenId] = currentTime
+
+    def countUnexpiredTokens(self, currentTime: int) -> int:
+        unexpired = 0
+        for t in self.tokens.values():
+            if t + self.time > currentTime:
+                unexpired += 1
+        return unexpired 
+
+
+# Your AuthenticationManager object will be instantiated and called as such:
+# obj = AuthenticationManager(timeToLive)
+# obj.generate(tokenId,currentTime)
+# obj.renew(tokenId,currentTime)
+# param_3 = obj.countUnexpiredTokens(currentTime)
