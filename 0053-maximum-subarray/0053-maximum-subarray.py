7dce8cf3bd05fb684e664c9802ffbaadf20572c7
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-10-20 20:04:21 -0400

    Time: 629 ms (22.54%), Space: 30.5 MB (26.39%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,25 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-#the brute force algorithm would take two nested for loops and have a running sum of numbers and keep track of the maximum sum until you've iterated through the whole array 
-        maxSum = nums[0]
-        currSum = 0
-        
-        for i in nums:
-            if currSum < 0:
-                currSum = 0
-            currSum += i 
-            maxSum = max(currSum, maxSum)
-            
-        return maxSum
-    
-'''
-nums = [5,4,-1,7,8]
-i=5, currSum=5, maxSum=5
-i=4, currSum=9, maxSum=9
-i=-1, currSum=8, maxSum=9
-i=7, currSum=15, maxSum=15
-i=8, currSum=23, maxSum=23
-'''
-                
-        
-        
+        maxSum = nums[0] 
+
+        for i in range(1,len(nums)): 
+            nums[i] = max(nums[i-1]+nums[i],nums[i])
+
+            maxSum = max(maxSum, nums[i])
+
+        return maxSum 
