0df0752eb8fd5eaa7a1e605ec1c79189b55884a2
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-10-28 17:22:58 -0400

    Time: 643 ms (51.99%), Space: 51.6 MB (20.15%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,16 +1,14 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # Make a grid of 0s with len(text2)+1 columns and len(text1)+1 rows
         dp = [[0]*(len(text2)+1) for _ in range(len(text1)+1)]
 
-        # Iterate up each column, starting from the last one
-        for col in reversed(range(len(text2))):
-            for row in reversed(range(len(text1))):
-                if text2[col]==text1[row]: #Characters are equal
-                    dp[row][col] = 1 + dp[row+1][col+1]
+        for i in range(len(text1)):
+            for j in range(len(text2)):
+                if text1[i] == text2[j]:
+                    dp[i+1][j+1] = dp[i][j] + 1 
                 else:
-                    dp[row][col] = max(dp[row+1][col], dp[row][col+1])
-        
-        return dp[0][0]
-        
+                    dp[i+1][j+1] = max(dp[i+1][j],dp[i][j+1])
 
+        print(dp)
+
+        return dp[len(text1)][len(text2)]
