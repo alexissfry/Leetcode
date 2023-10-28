67619f4ec97f7762db01721118a1d3dca597c1f6
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-10-28 16:59:30 -0400

    Time: 1021 ms (81.4%), Space: 55.8 MB (22.77%) - LeetHub

--- 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.py
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.py
@@ -1,26 +1,27 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        
+        #Build out a tree/graph to represent managers and their employees 
         relations = {}
 
         for i in range(n):
-            boss = manager[i]
-            if boss in relations:
-                relations[boss].append(i)
+            if manager[i] in relations:
+                relations[manager[i]].append(i)
             else:
-                relations[boss] = []
-                relations[boss].append(i)
+                relations[manager[i]] = []
+                relations[manager[i]].append(i)
+
+        #Find the longest path from root of the headID manager to the furthest away employee, this will give the total time it takes to inform all employees 
 
         def dfs(currID):
+            #If we are at an employee that has no subordinates, stop searching 
             if currID not in relations:
-                return 0
-
+                return informTime[currID]
+            
             totTime = 0
-            for ids in relations[currID]:
-                totTime = max(totTime, dfs(ids))
+            for e in relations[currID]:
+                totTime = max(dfs(e),totTime)
             
             return totTime + informTime[currID]
 
-
         return dfs(headID)
-
-        
