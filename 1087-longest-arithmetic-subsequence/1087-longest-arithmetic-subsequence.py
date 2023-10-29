934f37ce3417885f4bbdec1cf44af89c8eb2feb6
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-10-28 20:57:37 -0400

    Time: 4087 ms (5.68%), Space: 60.4 MB (39%) - LeetHub

---
+++ 1087-longest-arithmetic-subsequence/1087-longest-arithmetic-subsequence.py
@@ -0,0 +1,32 @@
+class Solution:
+    def longestArithSeqLength(self, nums: List[int]) -> int:
+        dp = {}
+        maxLen = 0
+
+        for i in range(len(nums)):
+            for j in range(0, i):
+                d = nums[i]-nums[j]
+                if (j,d) in dp:
+                    dp[(i,d)] = dp[(j,d)]
+                else:
+                    dp[(i,d)] = 0
+
+                dp[(i,d)] += 1 
+                maxLen = max(maxLen, dp[(i,d)])
+
+        return maxLen+1
+
+'''
+[1, 2, 0, 1, 2, 3]
+[... j... i]
+
+dp[(j,d)] = x # there is a longest arithmetic subsequence of length x ending at nums[j] with difference d
+AND I know that nums[i]-nums[j] = d
+
+dp[(i,d)] = the longest arithmetic subsequence that ends at nums[i] with a difference of d
+'''
+
+
+        
+
+        
