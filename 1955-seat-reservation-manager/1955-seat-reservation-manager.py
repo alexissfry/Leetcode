d4ca9f068d2fc007a02e95428872586a2b4ebc04
Alexis Fry <44038559+alexissfry@users.noreply.github.com>
2023-10-28 22:06:13 -0400

    Time: 459 ms (38.26%), Space: 51.1 MB (7.32%) - LeetHub

---
+++ 1955-seat-reservation-manager/1955-seat-reservation-manager.py
@@ -0,0 +1,53 @@
+from heapq import heapify, heappop, heappush
+# heapify(list) --> turn list into a heap in-place (akin to list.sort())
+# heappop(heap) --> heap pop from the list and return the smallest value
+# heappupsh(heap, val) --> heap push val onto the list
+
+class SeatManager:
+
+    def __init__(self, n: int):
+        self.n=n
+        seats = set()
+        self.seats = seats
+    
+        self.seatHeap = list(range(1,n+1))
+        heapify(self.seatHeap)
+
+    def reserve(self) -> int:
+        if len(self.seatHeap) != 0:
+            seatNum = heappop(self.seatHeap)
+            self.seats.add(seatNum)
+            return seatNum
+
+        return -1
+
+    def unreserve(self, seatNumber: int) -> None:
+        if seatNumber in self.seats:
+            self.seats.remove(seatNumber)
+            heappush(self.seatHeap,seatNumber)
+        
+        
+
+'''
+Edgecase behavior:
+- If seats are all reserved and another user tries to reserve, return -1
+
+["SeatManager", "reserve", "reserve", "unreserve", "reserve"]
+[[5], [], [], [2], []]
+
+seats = {1:False, 2:False, 3:False, 4:False, 5:False}
+
+seats = {1:True, 2:False, 3:False, 4:False, 5:False}
+
+seats= {1:True, 2:True, 3:False, 4:False, 5:False}
+
+seats= {1:True, 2:False, 3:False, 4:False, 5:False}
+
+seats= {1:True, 2:True, 3:False, 4:False, 5:False}
+
+'''
+
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
